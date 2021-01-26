36f56fe72ec03db26182bcc7fd33ddc4bfa10125
jrtwheeler <58869889+jrtwheeler@users.noreply.github.com>
2021-01-26 13:55:23 -0500

    Server location set to localhost 3001. Package.json proxy server set to localhost 3001.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const app = express();
 const apiRoutes = require("./routes/apiRoutes");
 
