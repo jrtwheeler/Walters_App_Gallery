4eaae2c64b5ec4ca29d4571d466e15a9d9c91f09
SeeMoreGold <moreau.cj@gmail.com>
2021-01-25 19:22:01 -0500

    Carousel and start of side nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Landing from "./pages/Landing";
 import SignUp from "./pages/SignUp";
 import LogIn from "./pages/LogIn";
-import NavBar from "./components/NavBar";
+
+import NavigationBar from "./components/NavigationBar";
+import SideBar from "./components/SideBar";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
 import { library } from '@fortawesome/fontawesome-svg-core'
@@ -16,12 +18,15 @@ function App() {
   return (
     <Router>
       <div>
-        <NavBar />
+        <NavigationBar />
+        <SideBar />
         <Wrapper>
+          <Switch>
           <Route exact path="/" component={Landing} />
           <Route exact path="/landing" component={Landing} />
           <Route exact path="/signup" component={SignUp} />
           <Route exact path="/login" component={LogIn} />
+          </Switch>
         </Wrapper>
         <Footer />
       </div>
