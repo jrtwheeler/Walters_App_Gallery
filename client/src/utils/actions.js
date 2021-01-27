da3b272f154e716b0591f1d612103bd6ee3dccfa
jrtwheeler <58869889+jrtwheeler@users.noreply.github.com>
2021-01-27 11:41:14 -0500

    Action.js file was built and added to the utils folder.

---
+++ client/src/utils/actions.js
@@ -0,0 +1,9 @@
+export const UPDATE_POSTS = "UPDATE_POSTS";
+export const ADD_POST = "ADD_POST";
+export const REMOVE_POST = "REMOVE_POST";
+export const GET_POSTS = "SET_CURRENT_POST";
+export const LOADING = "LOADING";
+
+export const ADD_FAVORITE = "REMOVE_POST";
+export const REMOVE_FAVORITE = "SET_CURRENT_POST";
+export const UPDATE_FAVORITE = "LOADING";
