b994b32b8865a726049e760f0e0e07ad75ee75c0
jrtwheeler <58869889+jrtwheeler@users.noreply.github.com>
2021-01-27 22:59:10 -0500

    Removed unused components from index.js and Navigation Bar component

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import Col from "../Col";
 import "./style.css";
 
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import Hero from "../components/Hero";
 import Container from "../components/Container";
 import Row from "../components/Row";
